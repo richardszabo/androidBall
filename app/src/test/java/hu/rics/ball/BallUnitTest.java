3b34362f641524b55cde6f8a3e667d356f6acea1
Rics <richard_szabo@jataka.hu>
2017-04-26 15:19:50 +0200

    Testing that ball position does not change if force, acceleration, or velocity are calculated.

--- app/src/test/java/hu/rics/ball/BallUnitTest.java
+++ app/src/test/java/hu/rics/ball/BallUnitTest.java
@@ -1,19 +1,29 @@
 package hu.rics.ball;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BallUnitTest {
+    Ball ball;
     double EPSILON = 0.01;
     float EXECUTION_RATE = 10.0f;
     int SAMPLE_WIDTH = 150;
     int SAMPLE_HEIGHT = 200;
+    float SAMPLE_POS_X = 40.0f;
+    float SAMPLE_POS_Y = 70.0f;
+    float SAMPLE_ANGLE_X = 10.0f;
+    float SAMPLE_ANGLE_Y = 20.0f;
+
+    @Before
+    public void setup() {
+        ball = new Ball();
+        ball.setArenaSize(SAMPLE_WIDTH,SAMPLE_HEIGHT);
+    }
 
     @Test
     public void resetPosition_isCorrect() {
-        Ball ball = new Ball();
-        ball.setArenaSize(SAMPLE_WIDTH,SAMPLE_HEIGHT);
         ball.resetPosition();
         assertEquals(ball.posX, SAMPLE_WIDTH/2,EPSILON);
         assertEquals(ball.posY, SAMPLE_HEIGHT/2,EPSILON);
@@ -21,7 +31,6 @@ public class BallUnitTest {
 
     @Test
     public void updatePosition_posXBelowZero() {
-        Ball ball = new Ball();
         ball.posX = -1;
         ball.updatePosition(EXECUTION_RATE);
         assertEquals(ball.posX, 0,EPSILON);
@@ -29,7 +38,6 @@ public class BallUnitTest {
 
     @Test
     public void updatePosition_posYBelowZero() {
-        Ball ball = new Ball();
         ball.posY = -1;
         ball.updatePosition(EXECUTION_RATE);
         assertEquals(ball.posY, 0,EPSILON);
@@ -37,8 +45,6 @@ public class BallUnitTest {
 
     @Test
     public void updatePosition_posXOverLimit() {
-        Ball ball = new Ball();
-        ball.setArenaSize(SAMPLE_WIDTH,SAMPLE_HEIGHT);
         ball.posX = SAMPLE_WIDTH + 1;
         ball.updatePosition(EXECUTION_RATE);
         assertEquals(ball.posX, SAMPLE_WIDTH, EPSILON);
@@ -46,11 +52,35 @@ public class BallUnitTest {
 
     @Test
     public void updatePosition_posYOverLimit() {
-        Ball ball = new Ball();
-        ball.setArenaSize(SAMPLE_WIDTH,SAMPLE_HEIGHT);
         ball.posY = SAMPLE_HEIGHT + 1;
         ball.updatePosition(EXECUTION_RATE);
         assertEquals(ball.posY, SAMPLE_HEIGHT, EPSILON);
     }
 
+    @Test
+    public void calculateForce_doesNotChangePosition() {
+        ball.posX = SAMPLE_POS_X;
+        ball.posY = SAMPLE_POS_Y;
+        ball.calculateForce(SAMPLE_ANGLE_X,SAMPLE_ANGLE_Y);
+        assertEquals(ball.posX, SAMPLE_POS_X, EPSILON);
+        assertEquals(ball.posY, SAMPLE_POS_Y, EPSILON);
+    }
+
+    @Test
+    public void calculateAcceleration_doesNotChangePosition() {
+        ball.posX = SAMPLE_POS_X;
+        ball.posY = SAMPLE_POS_Y;
+        ball.calculateAcceleration();
+        assertEquals(ball.posX, SAMPLE_POS_X, EPSILON);
+        assertEquals(ball.posY, SAMPLE_POS_Y, EPSILON);
+    }
+
+    @Test
+    public void updateVelocity_doesNotChangePosition() {
+        ball.posX = SAMPLE_POS_X;
+        ball.posY = SAMPLE_POS_Y;
+        ball.updateVelocity(EXECUTION_RATE);
+        assertEquals(ball.posX, SAMPLE_POS_X, EPSILON);
+        assertEquals(ball.posY, SAMPLE_POS_Y, EPSILON);
+    }
 }
